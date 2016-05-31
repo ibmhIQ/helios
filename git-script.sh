a79c5a9cb6dd92236f808d6910a7b54f82f8e291
root <root@psych-IT>
2016-05-31 21:35:49 +0200

    Updated git-script to restart celeryd on push

--- git-script.sh
+++ git-script.sh
@@ -5,8 +5,11 @@ if [ "$string_pull" != "Already up-to-date." ]; then
 	#date
 	#echo "HEAD UPDATED TO $string_head_hash_compare"
 	supervisorctl stop helios1
+	supervisorctl stop helios1celeryd
 	pkill -f "python manage.py runserver"
+	pkill -f "python manage.py celeryd"
 	supervisorctl start helios1
+	supervisorctl start helios1celeryd
 	
 fi
 
