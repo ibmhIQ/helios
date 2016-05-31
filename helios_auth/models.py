083e89a02b49443bcde463fb894371cc6262ad4a
fk176 <floriankadner@web.de>
2016-05-31 23:06:35 +0200

    FK 05/31/2016 23:06

--- helios_auth/models.py
+++ helios_auth/models.py
@@ -32,6 +32,9 @@ class User(models.Model):
   
   # administrator
   admin_p = models.BooleanField(default=False)
+  
+  # proxy
+  proxy = models.BooleanField(default=False)
 
   class Meta:
     unique_together = (('user_type', 'user_id'),)
